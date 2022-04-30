fb8a98bd8891bc349883d1fb7a66022894d0e05c
DavideVigano <davide.vigano2@studenti.unimi.it>
2022-04-30 18:55:52 +0200

    mod

--- script.js
+++ script.js
@@ -11,26 +11,43 @@ async function getCoordinates(name){
 
     let jsonObj = await response.json();
 
-    var  coordinates = {
+    let coordinates = {
         lat: jsonObj[0].lat,
         lon: jsonObj[0].lon,
     }
-
-    console.log(coordinates);
-
-    return coordinates;
+    return coordinates
 }
+async function getWether(c, units) {
 
-async function getWether(c) {
-    console.log(c);
-    let response = await fetch ("https://api.openweathermap.org/data/2.5/weather?lat="+c.lat+"&lon="+c.lon+"&appid=1dca80949b57093744a6ebd31d5c974b&units=metric",{
+    if (units == "C") {
+        units = "metric" 
+    }else{
+        units = "standard"
+    }
+
+    let response = await fetch ("https://api.openweathermap.org/data/2.5/weather?lat="+c.lat+"&lon="+c.lon+"&appid=1dca80949b57093744a6ebd31d5c974b&units="+units,{
         method: "GET"
     });
     var jsonObj = await response.json();
-    console.log(jsonObj);
+    /*
     let temp = jsonObj.main.temp;
     temp = temp.toFixed(0);
     document.getElementById("milanTemp").innerText = temp + "°";
+    */
+    
+    return jsonObj;
 }
 
-getWether(getCoordinates("Milano"));
+async function insertDescriptionWeatherByName(units,city){
+
+    var coordinates = await getCoordinates(city);
+    jsonWeather = await getWether(coordinates,units);
+    console.log(jsonWeather);
+    
+    document.getElementById("card1-header").innerText = city + ", " + jsonWeather.sys.country;
+
+}
+
+let units = "C";
+let city = "Milano";
+insertDescriptionWeatherByName(units,city);
