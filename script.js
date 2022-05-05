8e4b5767307a77ec75b7f4e17d5c72d1f3acf8e8
DavideVigano <davide.vigano2@studenti.unimi.it>
2022-05-05 14:38:02 +0200

    commit

--- script.js
+++ script.js
@@ -29,12 +29,7 @@ async function getWether(c, units) {
         method: "GET"
     });
     var jsonObj = await response.json();
-    /*
-    let temp = jsonObj.main.temp;
-    temp = temp.toFixed(0);
-    document.getElementById("milanTemp").innerText = temp + "°";
-    */
-    
+
     return jsonObj;
 }
 
@@ -43,11 +38,107 @@ async function insertDescriptionWeatherByName(units,city){
     var coordinates = await getCoordinates(city);
     jsonWeather = await getWether(coordinates,units);
     console.log(jsonWeather);
-    
+
+    //name country
     document.getElementById("card1-header").innerText = city + ", " + jsonWeather.sys.country;
+
+    //temperature
+    let temp = jsonWeather.main.temp;
+    temp = temp.toFixed(0);
+    document.getElementById("card1-temperature").innerText = temp + "°";
+
+    //basic info
+    document.getElementById("card1-info").innerText = jsonWeather.weather[0].main + ", " + jsonWeather.weather[0].description;
+
+    //name country card2
+    document.getElementById("card2-header").innerText = "Il meteo di oggi a "+ city + " " + jsonWeather.sys.country;
+
+    //temperature card2
+    document.getElementById("card2-temperature").innerText = temp + "°";
+
+    //info precise
+    let feels = jsonWeather.main.feels_like;
+    feels = feels.toFixed(0);
+
+    document.getElementById("feels-like").innerText = feels + "°";
+    document.getElementById("humidity").innerText = jsonWeather.main.humidity + " %";
+    document.getElementById("pressure").innerText = jsonWeather.main.pressure + " mb";
+
+    let min = jsonWeather.main.temp_max;
+    let max = jsonWeather.main.temp_min;
+    max = max.toFixed(0);
+    min = min.toFixed(0);
+    document.getElementById("max-min").innerText = max +"°/"+ min + " °" ;
+    document.getElementById("visibility").innerText = jsonWeather.visibility + " km";
+    document.getElementById("wind").innerText = jsonWeather.wind.speed + " km/h";
+
 
 }
 
+async function insertDescriptionWeatherByCoordinates(units,coordinates){
+
+    jsonWeather = await getWether(coordinates,units);
+
+    //name country
+    document.getElementById("card1-header").innerText = jsonWeather.name; + ", "+jsonWeather.sys.country;
+
+    //temperature
+    let temp = jsonWeather.main.temp;
+    temp = temp.toFixed(0);
+    document.getElementById("card1-temperature").innerText = temp + "°";
+
+    //basic info
+    document.getElementById("card1-info").innerText = jsonWeather.weather[0].main + ", " + jsonWeather.weather[0].description;
+
+    //name country card2
+    document.getElementById("card2-header").innerText = "Il meteo di oggi a "+ jsonWeather.name + " " + jsonWeather.sys.country;
+
+    //temperature card2
+    document.getElementById("card2-temperature").innerText = temp + "°";
+
+    //info precise
+    let feels = jsonWeather.main.feels_like;
+    feels = feels.toFixed(0);
+
+    document.getElementById("feels-like").innerText = feels + "°";
+    document.getElementById("humidity").innerText = jsonWeather.main.humidity + " %";
+    document.getElementById("pressure").innerText = jsonWeather.main.pressure + " mb";
+
+    let min = jsonWeather.main.temp_max;
+    let max = jsonWeather.main.temp_min;
+    max = max.toFixed(0);
+    min = min.toFixed(0);
+    document.getElementById("max-min").innerText = max +"°/"+ min + " °" ;
+    document.getElementById("visibility").innerText = jsonWeather.visibility + " km";
+    document.getElementById("wind").innerText = jsonWeather.wind.speed + " km/h";
+
+
+}
+
+async function insertHourlyForcastByCoordinates(units,coordinates){
+    let response = await fetch("https://api.openweathermap.org/data/2.5/forecast?lat="+coordinates.lat+"&lon="+coordinates.lon+"&appid=1dca80949b57093744a6ebd31d5c974b&units="+units,{
+        method: "GET"
+    });
+
+    var jsonWeather = await response.json();
+    
+    console.log(jsonWeather.list[0].dt_txt);
+
+    jsonWeather.list.forEach(dt_txt => {
+        console.log(dt_txt);
+    });
+    
+
+}
 let units = "C";
 let city = "Milano";
-insertDescriptionWeatherByName(units,city);
+
+let coordinates = {
+    lat: 45.4642,
+    lon: 9.1898,
+}
+
+//insertDescriptionWeatherByName(units,city);
+insertDescriptionWeatherByCoordinates(units,coordinates);
+insertHourlyForcastByCoordinates(units,coordinates);
+
