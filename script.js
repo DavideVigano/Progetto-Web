38bce491d3d777309fde4ab42a87f03d36a6b478
DavideVigano <davide.vigano2@studenti.unimi.it>
2022-05-10 13:00:15 +0200

    new svg

--- script.js
+++ script.js
@@ -1,7 +1,44 @@
+document.addEventListener('DOMContentLoaded', start());
 //change visual mode 
 document.getElementById("change-mode").onclick = function () {
     var element = document.body;
     element.classList.toggle("dark-mode");
+    var obj = localStorage.getItem('preferances');
+
+    obj = JSON.parse(obj);
+    if (obj.darkMode == true) {
+        obj.darkMode = false;
+
+    } else {
+        obj.darkMode = true;
+        console.log(obj);
+    }
+    obj = JSON.stringify(obj);
+    localStorage.setItem("preferances",obj);
+}
+
+//salvare preferenza dark-light mode
+function start() {
+    //verifico se oggetto presente in local storage
+    var obj = localStorage.getItem('preferances');
+    if (obj) {
+        // oggetto presente in local storage lo prendo e lo carico
+        obj = JSON.parse(obj);
+        if (obj.darkMode == true) {
+            document.body.classList.toggle("dark-mode");
+            var toggle = document.getElementById("change-mode");
+            toggle.checked = true;
+        }
+    }else{
+        //oggetto non presento, lo creo 
+        var preferance = {
+            darkMode: false,
+            //date: new Date().toString(),
+        }
+        //salvo oggetto in local storage
+        preferance = JSON.stringify(preferance);
+        localStorage.setItem("preferances",preferance);
+    }
 }
 
 async function getCoordinates(name){
@@ -17,6 +54,7 @@ async function getCoordinates(name){
     }
     return coordinates
 }
+
 async function getWether(c, units,lang) {
 
     if (units == "C") {
@@ -139,7 +177,6 @@ async function insertHourlyForcastByCoordinates(units,coordinates,lang){
 
     // take the first 5 elements
     let arrayJsonObject =  getNElemnt(5,jsonWeather);
-    //console.log(arrayJsonObject);
 
     //insert the hours
     document.getElementById("hour-c3-c1").innerText = 'Ora';
@@ -182,9 +219,9 @@ async function insertHourlyForcastByCoordinates(units,coordinates,lang){
     document.getElementById("temperature-c3-c5").innerText = temperature + '°';
 
     // immage
-    var url = "http://openweathermap.org/img/wn/";
-    var dim = "@4x.png";
-
+    var url = "Images/Icon-weather/";
+    var dim = ".svg";
+    console.log(arrayJsonObject[0]);
     let immageCode = arrayJsonObject[0].weather[0].icon;
     immageCode = url + immageCode + dim ;
     document.getElementById("immage-c3-c1").setAttribute("src",immageCode);
@@ -208,22 +245,27 @@ async function insertHourlyForcastByCoordinates(units,coordinates,lang){
     //probability rain 
     let probability = arrayJsonObject[0].pop;
     probability = probability * 100;
+    probability = probability.toFixed(0);
     document.getElementById("rain-c3-c1").innerText = probability + '%';
 
     probability = arrayJsonObject[1].pop;
     probability = probability * 100;
+    probability = probability.toFixed(0);
     document.getElementById("rain-c3-c2").innerText = probability + '%';
 
     probability = arrayJsonObject[2].pop;
     probability = probability * 100;
+    probability = probability.toFixed(0);
     document.getElementById("rain-c3-c3").innerText = probability + '%';
 
     probability = arrayJsonObject[3].pop;
     probability = probability * 100;
+    probability = probability.toFixed(0);
     document.getElementById("rain-c3-c4").innerText = probability + '%';
 
     probability = arrayJsonObject[4].pop;
     probability = probability * 100;
+    probability = probability.toFixed(0);
     document.getElementById("rain-c3-c5").innerText = probability + '%';
 }
 
@@ -234,6 +276,40 @@ function getNElemnt(N,jsonObject) {
     }
     return arrayJsonObject
 }
+
+async function insertAirQualityByCordinates(coordinates,time) {
+    let response = await fetch("http://api.openweathermap.org/data/2.5/air_pollution/history?lat="+coordinates.lat+"&lon="+coordinates.lon+"&start="+time.start+"&end="+time.end+"&appid=1dca80949b57093744a6ebd31d5c974b",{
+        method: "GET"
+    });
+
+    let jsonObj = await response.json();
+    let index = jsonObj.list[0].main.aqi;
+    index = getStateOfAirQualityIndex(index);
+    document.getElementById("index-c4").innerText = index;
+
+    let value = jsonObj.list[0].components.pm2_5;
+    console.log(jsonObj.list[0].components);
+    value = value.toFixed(0);
+    document.getElementById("value-c4").innerText = value + "%";
+}
+
+function getStateOfAirQualityIndex(index) {
+    switch (index) {
+        case 1:
+            return "good";
+        case 2:
+            return "fair";
+        case 3:
+            return "moderate";
+        case 4:
+            return "poor";
+        case 5:
+            return "very poor";
+        default:
+            return "non valido";
+    }
+}   
+
 let units = "C";
 let city = "Milano";
 let lang = "it";
@@ -242,8 +318,43 @@ let coordinates = {
     lat: 45.4642,
     lon: 9.1898,
 }
+data = Date.now();
+let time = {
+    start: 1606488670,
+    end: 1606747870,
+}
 
+//geoloc
+function success(pos) {
+    let units = "C";
+    let lang = "it";
+    let crd = pos.coords;
+
+    let coordinates = {
+        lat: crd.latitude,
+        lon: crd.longitude,
+    }
+
+    insertDescriptionWeatherByCoordinates(units,coordinates,lang);
+    insertHourlyForcastByCoordinates(units,coordinates,lang);
+}
+  
+function error(err) {
+    console.warn(`ERROR(${err.code}): ${err.message}`);
+    //default cordinates
+    let units = "C";
+    let lang = "it";
+    let coordinates = {
+        lat: 45.4642,
+        lon: 9.1898,
+    }
+    insertDescriptionWeatherByCoordinates(units,coordinates,lang);
+    insertHourlyForcastByCoordinates(units,coordinates,lang);
+}
+
+navigator.geolocation.getCurrentPosition(success, error);
 //insertDescriptionWeatherByName(units,city);
-insertDescriptionWeatherByCoordinates(units,coordinates,lang);
-insertHourlyForcastByCoordinates(units,coordinates,lang);
+//insertDescriptionWeatherByCoordinates(units,coordinates,lang);
+
+insertAirQualityByCordinates(coordinates,time);
 
